22ab545caf563652ec2d7b6a9f216136fb05a0c9
Bombo <bombo96@chungwoon.ac.kr>
2023-09-20 20:21:37 +0900

    refactor : shell script 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,51 +1,34 @@
 if [ "$DEPLOYMENT_GROUP_NAME" == "codedeploy-user" ]
 then
   REPOSITORY=/home/dev-table/user
-  cd $REPOSITORY
-
-  JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
-  JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
-
-  CURRENT_PID=$(pgrep -f $APP_NAME)
-
-  if [ -z $CURRENT_PID ] #2
-  then
-    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
-  else
-    echo "> kill -15 $CURRENT_PID"
-    sudo kill -15 $CURRENT_PID
-    sleep 5
-  fi
-
-  echo "> $JAR_PATH 배포" #3
-
-  nohup java -jar \
-          -Dspring.profiles.active=dev \
-          build/libs/$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 elif [ "$DEPLOYMENT_GROUP_NAME" == "codedeploy-owner" ]
 then
   REPOSITORY=/home/dev-table/owner
-  cd $REPOSITORY
-
-  JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
-  JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
-
-  CURRENT_PID=$(pgrep -f $APP_NAME)
-
-  if [ -z $CURRENT_PID ] #2
-  then
-    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
-  else
-    echo "> kill -15 $CURRENT_PID"
-    sudo kill -15 $CURRENT_PID
-    sleep 5
-  fi
-
-  echo "> $JAR_PATH 배포" #3
-
-  nohup java -jar \
-          -Dspring.profiles.active=dev \
-          build/libs/$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+else
+  echo "Unknown DEPLOYMENT_GROUP_NAME: $DEPLOYMENT_GROUP_NAME"
+  exit 1
 fi
 
+cd "$REPOSITORY" || exit 1
 
+echo "현재 이동 된 REPOSITORY :  $REPOSITORY"
+
+JAR_NAME=$(ls build/libs/*.jar | grep '.jar' | tail -n 1)
+JAR_PATH=build/libs/$JAR_NAME
+
+CURRENT_PID=$(pgrep -f "$APP_NAME")
+
+if [ -z "$CURRENT_PID" ]
+then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  echo "> kill -15 $CURRENT_PID"
+  sudo kill -15 "$CURRENT_PID"
+  sleep 5
+fi
+
+echo "> $JAR_PATH 배포"
+
+nohup java -jar \
+        -Dspring.profiles.active=dev \
+        "$JAR_PATH" > nohup.out 2>&1 &
